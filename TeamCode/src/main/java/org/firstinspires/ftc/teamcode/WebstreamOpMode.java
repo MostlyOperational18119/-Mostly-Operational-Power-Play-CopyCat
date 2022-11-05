d8424ccfb59a193fee18d42e024b6989b5458ced
TedArmenston <63930022+TedArmenston@users.noreply.github.com>
2022-11-05 12:39:44 -0700

    Set up OpenCV

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebstreamOpMode.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.openftc.easyopencv.OpenCvCamera;
+
+public class WebstreamOpMode extends LinearOpMode {
+
+    
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+        }
+    }
+}
