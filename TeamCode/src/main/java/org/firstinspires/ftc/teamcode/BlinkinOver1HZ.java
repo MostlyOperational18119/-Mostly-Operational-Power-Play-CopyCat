0e77a2b35ec1cb297ab7918c38fa71e5fdef904d
DamienB325 <damien.brown15@outlook.com>
2022-12-20 14:12:53 -0800

    "Oops, misclick."

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlinkinOver1HZ.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlinkinOver1HZ.java
@@ -16,6 +16,32 @@ public class BlinkinOver1HZ extends DriveMethods {
     @Override
     public void runOpMode() {
         initBlinkinOnly();
+        while(opModeInInit()) {
+            currentRandNum = intrng(0,6);
+            switch(currentRandNum) {
+                case 0:
+                    setBlinkinColor(Variables.BlinkinColor.GREEN);
+                    break;
+                case 1:
+                    setBlinkinColor(Variables.BlinkinColor.YELLOW);
+                    break;
+                case 2:
+                    setBlinkinColor(Variables.BlinkinColor.BLUE);
+                    break;
+                case 3:
+                    setBlinkinColor(Variables.BlinkinColor.RED);
+                    break;
+                case 4:
+                    setBlinkinColor(Variables.BlinkinColor.ORANGE);
+                    break;
+                case 5:
+                    setBlinkinColor(Variables.BlinkinColor.PINK);
+                    break;
+                case 6:
+                    setBlinkinColor(Variables.BlinkinColor.RAINBOW);
+                    break;
+            }
+        }
         waitForStart();
 
         telemetry.addLine("\"Oops\", accidentally created this.");
