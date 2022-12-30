59b14a66aea0fff73929e4501f0cbe514fbc53de
DamienB325 <damien.brown15@outlook.com>
2022-12-30 11:16:25 -0800

    Added some stuff and things™

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlinkinOver1HZ.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlinkinOver1HZ.java
@@ -2,46 +2,48 @@ package org.firstinspires.ftc.teamcode;
 
 import static java.lang.Math.random;
 
-import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name="BlinkinOver1HZ", group="A")
 public class BlinkinOver1HZ extends DriveMethods {
     int currentRandNum = 0;
-
     public int intrng(int minNum, int maxNum) {
         return (int)((random() * maxNum) + minNum);
     }
+    public double doublerng(int minNum, int maxNum) {
+        return (random() * maxNum) + minNum;
+    }
+
 
     @Override
     public void runOpMode() {
         initBlinkinOnly();
-        while(opModeInInit()) {
-            currentRandNum = intrng(0,6);
-            switch(currentRandNum) {
-                case 0:
-                    setBlinkinColor(Variables.BlinkinColor.GREEN);
-                    break;
-                case 1:
-                    setBlinkinColor(Variables.BlinkinColor.YELLOW);
-                    break;
-                case 2:
-                    setBlinkinColor(Variables.BlinkinColor.BLUE);
-                    break;
-                case 3:
-                    setBlinkinColor(Variables.BlinkinColor.RED);
-                    break;
-                case 4:
-                    setBlinkinColor(Variables.BlinkinColor.ORANGE);
-                    break;
-                case 5:
-                    setBlinkinColor(Variables.BlinkinColor.PINK);
-                    break;
-                case 6:
-                    setBlinkinColor(Variables.BlinkinColor.RAINBOW);
-                    break;
-            }
-        }
+//        while(opModeInInit()) {
+//            currentRandNum = intrng(0,6);
+//            switch(currentRandNum) {
+//                case 0:
+//                    setBlinkinColor(Variables.BlinkinColor.GREEN);
+//                    break;
+//                case 1:
+//                    setBlinkinColor(Variables.BlinkinColor.YELLOW);
+//                    break;
+//                case 2:
+//                    setBlinkinColor(Variables.BlinkinColor.BLUE);
+//                    break;
+//                case 3:
+//                    setBlinkinColor(Variables.BlinkinColor.RED);
+//                    break;
+//                case 4:
+//                    setBlinkinColor(Variables.BlinkinColor.ORANGE);
+//                    break;
+//                case 5:
+//                    setBlinkinColor(Variables.BlinkinColor.PINK);
+//                    break;
+//                case 6:
+//                    setBlinkinColor(Variables.BlinkinColor.RAINBOW);
+//                    break;
+//            }
+//        }
         waitForStart();
 
         telemetry.addLine("\"Oops\", accidentally created this.");
